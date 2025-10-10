e71d654310278e866bda020e26c53cb5818c74a1
Edgar Badalyan <edgar.badalyan.1430@gmail.com>
2025-10-10 15:08:13 +0300

    fix

--- app/models/category.py
+++ app/models/category.py
@@ -1,14 +1,19 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
+from typing import List, TYPE_CHECKING
+from sqlalchemy import Integer, String, ForeignKey
+from sqlalchemy.orm import relationship, Mapped, mapped_column 
 from app.database import Base
 
+if TYPE_CHECKING:
+    from .user import User
+    from .product import Product
 
 class Category(Base):
     __tablename__ = "categories"
 
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, unique=True, index=True, nullable=False)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    name: Mapped[str] = mapped_column(String, unique=True, index=True, nullable=False)
 
-    owner_id = Column(Integer, ForeignKey("users.id"), nullable=False)
-    owner = relationship("User", back_populates="categories")
-    products = relationship("Product", back_populates="category") 
+    owner_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
+    owner: Mapped["User"] = relationship(back_populates="categories")
+    
+    products: Mapped[List["Product"]] = relationship(back_populates="category") 
